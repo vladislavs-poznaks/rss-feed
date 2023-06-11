ace4860c8edd0b9d8fc5e4ece4381a20d459dce0
Vladislavs Poznaks <vladislavs.poznaks@gmail.com>
2023-06-11 17:05:56 +0300

    Reformatter migrations

--- sql/schema/001_users.sql
+++ sql/schema/001_users.sql
@@ -1,8 +1,9 @@
 -- +goose Up
 
-CREATE TABLE users (
-    id UUID PRIMARY KEY,
-    name TEXT NOT NULL,
+CREATE TABLE users
+(
+    id         UUID PRIMARY KEY,
+    name       TEXT      NOT NULL,
     created_at TIMESTAMP NOT NULL,
     updated_at TIMESTAMP NOT NULL
 );
--- sql/schema/003_feeds.sql
+++ sql/schema/003_feeds.sql
@@ -1,12 +1,13 @@
 -- +goose Up
 
-CREATE TABLE feeds (
-    id UUID PRIMARY KEY,
-    user_id UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE,
-    name TEXT NOT NULL,
-    url TEXT UNIQUE NOT NULL,
-    created_at TIMESTAMP NOT NULL,
-    updated_at TIMESTAMP NOT NULL
+CREATE TABLE feeds
+(
+    id         UUID PRIMARY KEY,
+    user_id    UUID        NOT NULL REFERENCES users (id) ON DELETE CASCADE,
+    name       TEXT        NOT NULL,
+    url        TEXT UNIQUE NOT NULL,
+    created_at TIMESTAMP   NOT NULL,
+    updated_at TIMESTAMP   NOT NULL
 );
 
 -- +goose Down
